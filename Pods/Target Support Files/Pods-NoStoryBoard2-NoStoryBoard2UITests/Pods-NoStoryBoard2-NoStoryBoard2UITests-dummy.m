eb09c4bb62ff7061439d42e0c9cf009e54181128
ryan.hong <ryan.hong@vipshop.com>
2021-10-25 18:09:15 +0800

    clear

--- Pods/Target Support Files/Pods-NoStoryBoard2-NoStoryBoard2UITests/Pods-NoStoryBoard2-NoStoryBoard2UITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_NoStoryBoard2_NoStoryBoard2UITests : NSObject
-@end
-@implementation PodsDummy_Pods_NoStoryBoard2_NoStoryBoard2UITests
-@end
